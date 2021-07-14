d38adc62d19b0d9b402c5eeb0766e1a301f6039d
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-07-13 23:59:25 -0400

    Cleaned the code; and added comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,50 +1,52 @@
-
 alert("Hey! Am working! Wohoo!");
-// console.log("Hey! Am working! Wohoo!");
 
-// $(document).ready(function() {
-//   // --- our code goes here ---
-//   console.log("Yes, am ready.");
-// });
+/*---------------------------------------------Testing event handlers---------------------------*/
+/*
+$(document).ready(function() {
+   // --- our code goes here ---
+   console.log("Yes, am ready.");
+ });
 
-// document.addEventListener("dblclick", (event) => {
-//   console.log(event);
-//   // your code here
-//   let x = event.offsetX;
-//   let y = event.offsetY;
-//   console.log(x, y);
-// });
+document.addEventListener("dblclick", (event) => {
+  console.log(event);
+  // your code here
+  let x = event.offsetX;
+  let y = event.offsetY;
+  console.log(x, y);
+});
 
-// let counter = 0;
-// document.addEventListener("keyup", (event) => {
-//   console.log(event);
-//   // your code here
-//   const keyInput = event.key;  
-//   // your code here
-//   console.log(`Key pressed: ${keyInput}` )
-// });
+document.addEventListener("keyup", (event) => {
+  console.log(event);
+  // your code here
+  const keyInput = event.key;  
+  // your code here
+  console.log(`Key pressed: ${keyInput}` )
+});
 
-// document.addEventListener("keydown", (event) => {
-//   //console.log(event);
-//   const keyInput = event.key;  
-//   // your code here
-//   console.log(`Key pressed: ${keyInput}` )
-// });
+document.addEventListener("keydown", (event) => {
+  //console.log(event);
+  const keyInput = event.key;  
+  // your code here
+  console.log(`Key pressed: ${keyInput}` )
+});
+*/
 
-// $("#tweet-text").blur(function() {
-//     // --- our code goes here ---
-//     console.log("Wrote something in the input field, and then clicked outside the field and it lost its focus (blur).")
-    
-//   });
-
-$("#tweet-text").on("keypress", function () {
-  // --- our code goes here ---
-  console.log("Triggered kepress event, when a keyboard-button is pressed.")
-  console.log(this.value);
-
+/*-------------------------------------- Event-handlers on selectors using jquery-------------*/
+/*
+$("#tweet-text").blur(function () {
+ 
+  console.log("Wrote something in the input field, and then clicked outside the field and it lost its focus (blur).")
 });
 
 
+$("#tweet-text").on("keypress", function () {
+  
+  console.log("Triggered kepress event, when a keyboard-button is pressed.")
+  console.log(this.value);
+});
+*/
+
+/*-------------------------------------Showing characters left in the counter----------------*/
 
 /*
   $("#tweet-text").on("input", function() {
@@ -55,7 +57,7 @@ $("#tweet-text").on("keypress", function () {
     //finding the length of the input
     const tweetLength = this.value.length;
     
-    //
+    // Using find & next 
     let output = $(this).next().find('#counterID');
     // console.log(counter.value); //The this keyword is a reference to the button
     output["0"].defaultValue = 140 - tweetLength;
